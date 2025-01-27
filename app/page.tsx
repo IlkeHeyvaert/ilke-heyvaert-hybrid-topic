b9c77b0e6000092957f2ce4b1849f135b592f114
ilkeh <r0900651@student.thomasmore.be>
2025-01-27 14:25:19 +0100

    Fix milestone 1 version 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
       <div className="text-center min-h-screen h-full flex flex-col justify-center items-center gap-y-20 w-full">
