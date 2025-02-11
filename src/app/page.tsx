5ec0b32444df788fd545a4cb42dea021ec0b7b33
ilkeh <r0900651@student.thomasmore.be>
2025-02-11 17:48:51 +0100

    Add milestone 2: navigation and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Navigation } from "../components/navigation";
+
 export default function Home() {
   return (
-      <div className="text-center min-h-screen h-full flex flex-col justify-center items-center gap-y-20 w-full">
-        <h1 className="text-4xl font-bold">Ilke Heyvaert</h1>
-      </div>
+  <div className="min-h-screen flex flex-col">
+   <main className="flex-grow">
+        <Navigation />
+        
+        
+      </main>
+      <footer className="bg-gray-100 p-5 flex justify-center ">
+          <h1 className="text-xl font-nunito">Ilke Heyvaert</h1>
+        </footer>
+  </div>
+     
+        
   );
 }
---
+++ src/components/navigation.tsx
@@ -0,0 +1,6 @@
+export function Navigation(){
+    return (
+       <div className="from-[#F7BDC4] to-[#F1D898] w-full h-20 bg-gradient-to-r flex items-center pl-36">
+              <h1 className="text-gray-500 text-[40px] font-poppins">QuestPlanner</h1>
+       </div>   )
+}
